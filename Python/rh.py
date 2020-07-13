465f144d8a443981dd25ddd7cd18c368dad089df
Bray <braypolk@gmail.com>
2020-07-13 00:08:55 -0600

    purchase page and showing values

--- Python/rh.py
+++ Python/rh.py
@@ -1,6 +1,7 @@
 import robin_stocks as rh
 import json
 import random
+import requests
 from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ def home():
 @app.route("/login", methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        print(rh.login(request.form['username'], request.form['password']))
+        print(rh.login(request.form['username'], request.form['password'], 30))
         profile = rh.load_account_profile()
         return redirect(url_for('purchase'))
     return render_template('index.html')
@@ -25,11 +26,19 @@ def purchase():
     print(stockChoice)
 
     stockInfo = rh.find_instrument_data(stockChoice)
+    price = ""
     if(stockInfo[0].get('fractional_tradability') == "tradable"):
         print("SUCCESS")
         # timeInForce must be gfd
-        # print(rh.order_buy_fractional_by_price(symbol=stockChoice, amountInDollars=1.00, timeInForce='gfd'))
-    return render_template("overview.html", randomStock=stockChoice)
+        amount = 1.00
+        # purchase = rh.order_buy_fractional_by_price(symbol=stockChoice, amountInDollars=amount, timeInForce='gfd')
+        # price = purchase["price"]
+    return render_template("overview.html", randomStock=stockChoice, purchasePrice=price, dollarAmountPurchased=amount)
+
+@app.route("/logout")
+def logout():
+    rh.logout()
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug = True)
