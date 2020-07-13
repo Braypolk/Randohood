a7ca604006ca85e1fbb8d65dcd11c0ed5c9ef5f0
Bray <braypolk@gmail.com>
2020-07-12 18:21:42 -0600

    got flask rolling and link between login and purchasing stock

--- Python/rh.py
+++ Python/rh.py
@@ -1,20 +1,35 @@
 import robin_stocks as rh
 import json
 import random
+from flask import Flask, render_template, request, url_for, redirect
 
-# import stocks from json file and choose random stock
-with open('../stocks.json') as json_file:
-    stocks = json.load(json_file)
-stockChoice = random.choice(stocks)
-print(stockChoice)
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return redirect(url_for('login'))
 
-# login to users Robinhood account and purchase $5 worth of stock
-# TODO: get user info from sign in
-rh.login("braypolk@comcast.net")
-profile = rh.load_account_profile()
+@app.route("/login", methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        print(rh.login(request.form['username'], request.form['password']))
+        profile = rh.load_account_profile()
+        return redirect(url_for('purchase'))
+    return render_template('index.html')
 
-stockInfo = rh.find_instrument_data(stockChoice)
-if(stockInfo[0].get('fractional_tradability') == "tradable"):
-    print("SUCCESS")
-    # timeInForce must be gfd
-    print(rh.order_buy_fractional_by_price(symbol=stockChoice, amountInDollars=1.00, timeInForce='gfd'))
+@app.route("/purchase")
+def purchase():
+    # import stocks from json file and choose random stock
+    with open('../stocks.json') as json_file:
+        stocks = json.load(json_file)
+    stockChoice = random.choice(stocks)
+    print(stockChoice)
+
+    stockInfo = rh.find_instrument_data(stockChoice)
+    if(stockInfo[0].get('fractional_tradability') == "tradable"):
+        print("SUCCESS")
+        # timeInForce must be gfd
+        # print(rh.order_buy_fractional_by_price(symbol=stockChoice, amountInDollars=1.00, timeInForce='gfd'))
+    return render_template("overview.html", randomStock=stockChoice)
+
+if __name__ == "__main__":
+    app.run(debug = True)
