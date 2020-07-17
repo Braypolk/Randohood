9f240ada075de098e3aa6b06dce6984dd85fea3d
Braypolk <braypolk@gmail.com>
2020-07-17 18:33:16 +0000

    js api won't login

--- jsVersion/app.js
+++ jsVersion/app.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000
+const port = 8080
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}/templates/index.html`))
+app.use(express.static(__dirname +'/static/'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', (req, res) => res.sendFile(`${__dirname}/templates/index.html`));
+
+app.post('/api/data', function(req, res){
+    console.log(req.body)
+    var credentials = {
+        username: 'braypolk@comcast.net',
+        password: 'Bray89511!'
+    }
+    var Robinhood = require('robinhood')(credentials, function(){
+
+    //Robinhood is connected and you may begin sending commands to the api.
+
+    Robinhood.quote_data('GOOG', function(error, response, body) {
+        if (error) {
+            console.error(error);
+            process.exit(1);
+        }
+        console.log(body);
+    });
+
+});
+    res.end()
+});
+
+function updateValues(){
+    document.getElementById('purchasedAmount').innerHTML = `You Purchased ${dollarAmountPurchased} worth of`;
+    document.getElementById('randomStock').innerHTML = `${randomStock}`;
+    document.getElementById('purchasePrice').innerHTML = `\$${purchasePrice}`;
+    document.getElementById('dollarAmountPurchased').innerHTML = `Just reload the page to "invest" another ${dollarAmountPurchased}`;
+}
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
